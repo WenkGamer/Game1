b2ec6886baf57703c79fab84b15216ebe32a76d6
WenkGamer <tranmanh100654174@gmail.com>
2025-07-25 19:35:38 +0700

    ll

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,27 +8,11 @@ public class Enemy : MonoBehaviour
 
     void Awake()
     {
-        agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();           
     }
 
     private void Start()
     {
-        agent.autoRepath = true;
-
-        if (target != null)
-        {
-            Vector3 hitPosition;
-            if (NavMesh.SamplePosition(transform.position, out NavMeshHit hit, 2f, NavMesh.AllAreas))
-            {
-                // Ép enemy về đúng vị trí trên NavMesh nếu lệch
-                transform.position = hit.position;
-                agent.SetDestination(target.position);
-                Debug.Log("Enemy snapped to NavMesh.");
-            }
-            else
-            {
-                Debug.LogError("Enemy is not on NavMesh!");
-            }
-        }
+        
     }
 }
