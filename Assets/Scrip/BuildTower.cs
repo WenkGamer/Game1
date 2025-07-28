51087ee3251c8e9abaed7d8c29b9cf8789f1efdf
WenkGamer <tranmanh100654174@gmail.com>
2025-07-29 02:33:23 +0700

    code 28/7
    tôi đẩy bị conflict r

---
+++ Assets/Scrip/BuildTower.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class BuildTower : MonoBehaviour
+{
+    public Transform spawn;
+    public GameObject towerSelectionUI;
+    private GameObject currentUI;
+
+    private void OnMouseDown()
+    {
+        if(currentUI == null)
+        {
+            currentUI = Instantiate(towerSelectionUI, transform.position, Quaternion.identity, spawn);
+
+            BuildCanvas ui = currentUI.GetComponent<BuildCanvas>();
+            if (ui != null)
+                ui.Setup(this);
+        }
+    }
+
+    public void BuildTowercl(GameObject towerPrefab)
+    {
+        Instantiate(towerPrefab, transform.position, Quaternion.identity);
+        Destroy(currentUI);
+        Destroy(gameObject);
+    }
+
+}
