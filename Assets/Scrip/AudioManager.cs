660de22f1d603709858000f9e1f8ce5ab2bcb48c
HoangDucManh05 <manhstudio2k5@gmail.com>
2025-08-04 20:19:46 +0700

    up 8/4

---
+++ Assets/Scrip/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider volumeSlider;
+
+    private void Start()
+    {
+        // Lấy volume đã lưu nếu có
+        float volume = PlayerPrefs.GetFloat("MusicVol_dB", 0f);
+        volumeSlider.value = volume;
+        SetMusicVolume(volume);
+    }
+
+    public void SetMusicVolume(float dB)
+    {
+        audioMixer.SetFloat("MusicVolume", dB);
+        PlayerPrefs.SetFloat("MusicVol_dB", dB);
+    }
+}
--- Assets/Scrip/Menu.cs
+++ Assets/Scrip/Menu.cs
@@ -1,12 +1,19 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    public Slider volumeSlider;
+    public GameObject Slide;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        float volume = PlayerPrefs.GetFloat("MusicVol_dB", 0f);
+        volumeSlider.value = volume;
+        SetMusicVolume(volume);
     }
 
     // Update is called once per frame
@@ -17,5 +24,35 @@ public class Menu : MonoBehaviour
     public void start()
     {
        SceneManager.LoadScene(1);
+    }
+
+    public void Setting()
+    {
+        /*bool isOpen = false;
+        isOpen = !isOpen;
+        if (isOpen)
+        {
+            Slide.SetActive(true);
+        }
+        else 
+        { 
+            Slide.SetActive(false); 
+        }*/
+        if (Slide.activeSelf)
+        {
+            Slide.SetActive(false);
+            Debug.Log("Lần 2: Tắt Slide");
+        }
+        else
+        {
+            Slide.SetActive(true);
+            Debug.Log("Lần 1: Mở Slide");
+        }
+
+    }
+    public void SetMusicVolume(float dB)
+    {
+        audioMixer.SetFloat("MusicVolume", dB);
+        PlayerPrefs.SetFloat("MusicVol_dB", dB);
     }
 }
