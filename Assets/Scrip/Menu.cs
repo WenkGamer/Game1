98cdef3d88bfaef6fcadc3b0075cf5c02eb9ba50
HoangDucManh05 <manhstudio2k5@gmail.com>
2025-08-08 12:39:29 +0700

    upcode

--- Assets/Scrip/Menu.cs
+++ Assets/Scrip/Menu.cs
@@ -1,21 +1,91 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    public Slider volumeSlider;
+    public GameObject Slide;
+    public GameObject MenuScreen;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        float volume = PlayerPrefs.GetFloat("MusicVol_dB", 0f);
+        volumeSlider.value = volume;
+        SetMusicVolume(volume);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void start()
     {
-       SceneManager.LoadScene(1);
+        SceneManager.LoadScene(1);
     }
+
+    public void Setting()
+    {
+        /*bool isOpen = false;
+        isOpen = !isOpen;
+        if (isOpen)
+        {
+            Slide.SetActive(true);
+        }
+        else 
+        { 
+            Slide.SetActive(false); 
+        }*/
+        if (Slide.activeSelf)
+        {
+            Slide.SetActive(false);
+            Debug.Log("Lần 2: Tắt Slide");
+        }
+        else
+        {
+            Slide.SetActive(true);
+            Debug.Log("Lần 1: Mở Slide");
+        }
+
+    }
+    public void SetMusicVolume(float dB)
+    {
+        audioMixer.SetFloat("MusicVolume", dB);
+        PlayerPrefs.SetFloat("MusicVol_dB", dB);
+    }
+
+    public void ScreenMenu()
+    {
+        MenuScreen.SetActive(true);
+    }
+
+    #region Screen
+    public void Screen1()
+    {
+
+    }
+    public void Screen2()
+    {
+
+    }
+    public void Screen3()
+    {
+
+    }
+    public void Screen4()
+    {
+
+    }
+    public void Screen5()
+    {
+
+    }
+    public void ExitMenuScreen()
+    {
+        MenuScreen.SetActive(false);
+    }
+    #endregion
 }
